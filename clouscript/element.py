272b5c179a2b23889231d1e23d6d2cfbc76446c3
deepadmax <maximillian.strand@gmail.com>
2021-04-04 20:33:09 +0200

    added element

---
+++ clouscript/element.py
@@ -0,0 +1,11 @@
+
+
+class Element:
+    """The most basic element in the code.
+    It represents anything from a function call
+    to an integer or a collection.
+    All types of elements inherit from this class.
+    """
+    
+    def __init__(self, value):
+        self.value = value
