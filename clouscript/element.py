491ea46fb2d5a807e27878be9124ecd62d41d757
deepadmax <maximillian.strand@gmail.com>
2021-04-04 23:40:13 +0200

    added type and __eq__ to element

--- clouscript/element.py
+++ clouscript/element.py
@@ -7,6 +7,15 @@ class Element:
     All types of elements inherit from this class.
     """
     
-    def __init__(self, name, value):
-        self.name = name
+    def __init__(self, type_, value):
+        self.type = type_
         self.value = value
+
+    def __eq__(self, value):
+        """
+        If a string is provided:
+            Check if element is a certain type
+        If an element is provided:
+            Check if the elements are equal in type and value
+        """
+        return self.type == value
