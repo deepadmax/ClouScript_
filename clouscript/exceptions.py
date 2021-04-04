739f8b4f3e938ef6bd872c91282ac59c84ce8877
deepadmax <maximillian.strand@gmail.com>
2021-04-05 00:49:25 +0200

    added more specific exceptions

--- clouscript/exceptions.py
+++ clouscript/exceptions.py
@@ -1,9 +1,17 @@
 
 
-class ClouScriptError(Exception): pass
+class ClouScriptException(Exception): pass
 
-class LexingError(ClouScriptError):
+
+class LexingError(ClouScriptException):
     """String is not compliant with lexing rules"""
 
-class ParsingError(ClouScriptError):
+class MissingDelimiter(LexingError):
+    """No instances of a specified delimiter"""
+
+class EmptySection(LexingError):
+    """Found empty section while segmenting"""
+
+
+class ParsingError(ClouScriptException):
     """Elements are not compliant with parsing rules"""
